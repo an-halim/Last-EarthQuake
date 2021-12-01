b1a6ec261f9862ad63d50832b4c05bfa3f5f3bf0
an-halim <halimbla2@gmail.com>
2021-12-01 08:49:07 +0700

    refactor

---
+++ func/index.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+
+module.exports.get_between = function (string, start, end) {
+  string = " " + string;
+  let ini = string.indexOf(start);
+  if (ini == 0) return "";
+  ini += start.length;
+  let len = string.indexOf(end, ini) - ini;
+  return string.substr(ini, len);
+};
+
+module.exports.JSON = function (gambar, magnitude, kedalaman, koordinat, lokasi, waktu) {
+  return `{
+      "gambar": "${gambar}", 
+      "magnitude": "${magnitude}", 
+      "kedalaman": "${kedalaman}", 
+      "koordinat": "${koordinat}", 
+      "lokasi": "${lokasi}", 
+      "waktu": "${waktu}"
+}`;
+};
+
+module.exports.readMe = function (gambar, magnitude, kedalaman, koordinat, lokasi, waktu) {
+  return `# Last-EarthQuake
+This repo will looking update every 5 minute with Last EarthQuake report from BMKG
+<br>
+<br>
+Last Update
+<br>
+<img src="${gambar}" width="400"></img>
+<br>
+<br>
+Lokasi: ${lokasi} <br>
+Magnitude: ${magnitude} <br>
+Kedalaman: ${kedalaman} <br>
+Koordinat: ${koordinat} <br>
+Waktu: ${waktu} <br>
+`;
+};
--- index.js
+++ index.js
@@ -1,61 +1,47 @@
-const axios = require('axios')
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
+const func = require("./func");
 
-function get_between(string, start, end) {
-    string = " " + string;
-    let ini = string.indexOf(start);
-    if (ini == 0) return "";
-    ini += start.length;
-    let len = string.indexOf(end, ini) - ini;
-    return string.substr(ini, len);
+async function getUpdate() {
+  let request = await axios.get("https://www.bmkg.go.id/gempabumi/gempabumi-dirasakan.bmkg");
+  data = func.get_between(request.data, "<td>1</td>", "<td>2</td>");
+  gambar = func.get_between(data, 'src="', '"');
+  magnitude = func.get_between(data, 'magnitude"></span>', "</li>");
+  kedalaman = func.get_between(data, 'kedalaman"></span>', "</li>");
+  koordinat = func.get_between(data, 'koordinat"></span>', "</li>");
+  lokasi = func.get_between(data, 'lokasi np"></span>', "</li>");
+  waktu = func.get_between(data, "<td>1</td>>", koordinat).slice(0, 26).replace("<br>", " ");
+  json = func.JSON(gambar, magnitude, kedalaman, koordinat, lokasi, waktu);
+  if (waktu == "" || waktu == null) json = await getUpdate();
+  return json;
 }
 
+(async () => {
+  try {
+    var notif = setInterval(function () {
+      console.log("Looking update....");
+    }, 200);
 
-(async() =>{
-    try {
-        let gempa = await axios.get("https://www.bmkg.go.id/gempabumi/gempabumi-dirasakan.bmkg");
-        data = get_between(gempa.data, '<td>1</td>', '<td>2</td>');
-        gambar = get_between(data, 'src="', '"');
-        magnitude = get_between(data, 'magnitude"></span>', '</li>');
-        kedalaman = get_between(data, 'kedalaman"></span>', '</li>');
-        koordinat = get_between(data, 'koordinat"></span>', '</li>');
-        lokasi = get_between(data, 'lokasi np"></span>', '</li>');
-        waktu = get_between(data, '<td>1</td>>', koordinat).slice(0, 26).replace("<br>", " ");
-        json = `{"gambar": "${gambar}", "magnitude": "${magnitude}", "kedalaman": "${kedalaman}", "koordinat": "${koordinat}", "lokasi": "${lokasi}", "waktu": "${waktu}"}`
-        fs.readFile("./data/data.json", (err, data) =>{
-            if(err) throw err
-            dataGempa = JSON.parse(data)
-            if (dataGempa.waktu != waktu) {
-                console.log("Update require!");
-                fs.writeFile("./data/data.json", json, function(err){
-                    if(err) throw err
-                    let readMe = `
-# Last-EarthQuake
-This repo will auto update every 5 minute with Last EarthQuake report from BMKG
-<br>
-<br>
-Last Update
-<br>
-<img src="${gambar}" width="400"></img>
-<br>
-<br>
-Magnitude: ${magnitude} <br>
-Kedalaman: ${kedalaman} <br>
-Koordinat: ${koordinat} <br>
-Waktu: ${waktu} <br>
-`
-                    fs.writeFile("./README.md", readMe, function(err){
-                        if(err) throw err
-                        console.log("Updated!");
-                    })
-                })
-            } else{
-                console.log("No update require!");
-            }
-        })
-        
-    } catch (error) {
-        throw error
-    }
-    
+    let gempa = await getUpdate();
+    fs.readFile("./data/data.json", (err, data) => {
+      if (err) throw err;
+      dataGempa = JSON.parse(data);
+      if (dataGempa.waktu != waktu) {
+        console.log("Update require!");
+        fs.writeFile("./data/data.json", gempa, function (err) {
+          if (err) throw err;
+          let readMe = func.readMe(gambar, magnitude, kedalaman, koordinat, lokasi, waktu);
+          fs.writeFile("./README.md", readMe, function (err) {
+            if (err) throw err;
+            console.log("Updated!");
+          });
+        });
+      } else {
+        console.log("No update require!");
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
+  clearInterval(notif);
 })();
